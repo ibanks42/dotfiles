ca14d1a8fb51f8ff4d1f6f92b372c02832090d2b
ibanks42 <isaiah.banks42@gmail.com>
2024-07-12 10:34:54 -0400

    some new plugins

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -4,6 +4,82 @@
 -- See the kickstart.nvim README for more information
 return {
   {
+    'akinsho/bufferline.nvim',
+    version = '*',
+    dependencies = { 'nvim-tree/nvim-web-devicons', 'famiu/bufdelete.nvim' },
+    config = function()
+      local bufferline = require 'bufferline'
+      bufferline.setup {
+        options = {
+          offsets = {
+            {
+              filetype = 'NvimTree',
+              text = 'File Explorer',
+              separator = true,
+              text_align = 'left',
+            },
+          },
+        },
+      }
+
+      vim.keymap.set('n', '<leader>bh', ':BufferLineCyclePrev<CR>',
+        { silent = true, noremap = true, desc = '[B]ufferLine Previous Buffer' })
+      vim.keymap.set('n', '<leader>bl', ':BufferLineCycleNext<CR>',
+        { silent = true, noremap = true, desc = '[B]ufferLine Next Buffer' })
+      vim.keymap.set('n', '<leader>bd', ':Bdelete<CR>', { silent = true, noremap = true, desc = '[B]uffer [D]elete' })
+    end,
+  },
+  {
+    'kdheepak/lazygit.nvim',
+    cmd = {
+      'LazyGit',
+      'LazyGitConfig',
+      'LazyGitCurrentFile',
+      'LazyGitFilter',
+      'LazyGitFilterCurrentFile',
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+    },
+  },
+  {
+    'lewis6991/gitsigns.nvim',
+    config = function()
+      require('gitsigns').setup {}
+    end,
+  },
+  {
+    'stevearc/resession.nvim',
+    opts = {},
+    config = function()
+      local resession = require 'resession'
+      resession.setup {}
+      vim.keymap.set('n', '<leader>ns', resession.save, { desc = 'Resessio[n] [S]ave' })
+      vim.keymap.set('n', '<leader>nl', resession.load, { desc = 'Resessio[n] [L]oad' })
+      vim.keymap.set('n', '<leader>nd', resession.delete, { desc = 'Resessio[n] [D]elete' })
+      vim.api.nvim_create_autocmd('VimLeavePre', {
+        callback = function()
+          if table.getn(resession.list {}) > 0 then
+            resession.save 'last'
+          end
+        end,
+      })
+      vim.api.nvim_create_autocmd('VimEnter', {
+        callback = function()
+          if vim.fn.argc(-1) == 0 then
+            resession.load 'last'
+          end
+        end,
+      })
+    end,
+  },
+  {
     'nvim-tree/nvim-tree.lua',
     version = '*',
     lazy = false,
