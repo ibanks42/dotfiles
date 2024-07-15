b8b7c2a22292c9fc181c1b3e4a3218d104ef45d2
ibanks42 <isaiah.banks42@gmail.com>
2024-07-15 10:27:17 -0400

    Remove resession and update deprecated which-key commands

--- nvim/init.lua
+++ nvim/init.lua
@@ -281,26 +281,51 @@ require('lazy').setup({
     'folke/which-key.nvim',
     event = 'VimEnter', -- Sets the loading event to 'VimEnter'
     config = function() -- This is the function that runs, AFTER loading
-      require('which-key').setup {
-        triggers_blacklist = {
-          n = { 'v' },
+      local wk = require 'which-key'
+      wk.setup {}
+
+      wk.add {
+        {
+          '<leader>c',
+          group = '[C]ode',
+          hidden = true,
+        },
+        {
+          '<leader>d',
+          group = '[D]ocument',
+          hidden = true,
+        },
+        {
+          '<leader>r',
+          group = '[R]ename',
+          hidden = true,
+        },
+        {
+          '<leader>s',
+          group = '[S]earch',
+          hidden = true,
+        },
+        {
+          '<leader>w',
+          group = '[W]orkspace',
+          hidden = true,
+        },
+        {
+          '<leader>t',
+          group = '[T]oggle',
+          hidden = true,
+        },
+        {
+          '<leader>h',
+          group = 'Git [H]unk',
+          hidden = true,
+        },
+        {
+          'n',
+          mode = 'v',
+          hidden = true,
         },
       }
-
-      -- Document existing key chains
-      require('which-key').register {
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-        ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-        ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
-      }
-      -- visual mode
-      require('which-key').register({
-        ['<leader>h'] = { 'Git [H]unk' },
-      }, { mode = 'v' })
     end,
   },
 
