0d2453babc9c0058437f918699ef1e1534c502a5
ibanks42 <isaiah.banks42@gmail.com>
2024-07-15 16:10:11 -0400

    wezterm install

--- install_bash.sh
+++ install_bash.sh
@@ -4,20 +4,31 @@
 REPO_URL="https://github.com/ibanks42/dotfiles.git" # Replace with your repository URL
 TEMP_PATH="/tmp/nvim-install"
 NVIM_CONFIG_PATH="$HOME/.config/nvim"
+WEZTERM_CONFIG_PATH="$HOME"
 
 # Function to clone the repository
 clone_repository() {
-    echo "Cloning repository from $REPO_URL..."
-    git clone "$REPO_URL" "$TEMP_PATH"
+    git clone -q "$REPO_URL" "$TEMP_PATH"
 }
 
 # Function to copy nvim configuration
 copy_nvim_config() {
-    echo "Copying nvim configuration to $NVIM_CONFIG_PATH..."
     if [ -d "$NVIM_CONFIG_PATH" ]; then
         rm -rf "$NVIM_CONFIG_PATH"
     fi
     cp -r "$TEMP_PATH/nvim" "$NVIM_CONFIG_PATH"
+
+    echo "-> Neovim configuration installed successfully."
+}
+
+# Function to copy wezterm configuration
+copy_wezterm_config() {
+    if [ -d "$WEZTERM_CONFIG_PATH" ]; then
+        rm -rf "$WEZTERM_CONFIG_PATH"
+    fi
+    cp -r "$TEMP_PATH/wezterm/." "$WEZTERM_CONFIG_PATH"
+
+    echo "-> Wezterm configuration installed successfully."
 }
 
 # Main script
@@ -34,5 +45,3 @@ copy_nvim_config
 
 # Cleanup
 rm -rf "$TEMP_PATH"
-
-echo "nvim configuration installed successfully."
