1106d16c88c437aead56b6e480d413e5294ec1a8
ibanks42 <isaiah.banks42@gmail.com>
2024-07-17 15:56:39 -0400

    delete keymap for harpoon & tsserver ensure installed

--- nvim/init.lua
+++ nvim/init.lua
@@ -596,8 +596,8 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
-        -- tsserver = {},
         --
+        tsserver = {},
         biome = {},
 
         lua_ls = {
@@ -629,6 +629,7 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'tsserver',
         'biome',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
