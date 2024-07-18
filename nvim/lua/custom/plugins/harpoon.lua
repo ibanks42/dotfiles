7be08fc030c9367f7f31bb000f90226721a9a342
ibanks42 <isaiah.banks42@gmail.com>
2024-07-18 15:13:13 -0400

    lazy load some plugins for faster startup

--- nvim/lua/custom/plugins/harpoon.lua
+++ nvim/lua/custom/plugins/harpoon.lua
@@ -2,19 +2,42 @@ return {
   'ThePrimeagen/harpoon',
   branch = 'harpoon2',
   dependencies = { 'nvim-lua/plenary.nvim' },
+  event = 'BufEnter',
   config = function()
     local harpoon = require 'harpoon'
 
     -- REQUIRED
     harpoon:setup()
     -- REQUIRED
+    local function remove(item)
+      local list = harpoon:list()
+      item = item or list.config.create_list_item(list.config)
+      local Extensions = require 'harpoon.extensions'
+      local Logger = require 'harpoon.logger'
+
+      local items = list.items
+      if item ~= nil then
+        for i = 1, list._length do
+          local v = items[i]
+          if list.config.equals(v, item) then
+            -- this clears list somehow
+            -- items[i] = nil
+            table.remove(items, i)
+            list._length = list._length - 1
+
+            Logger:log('HarpoonList:remove', { item = item, index = i })
+
+            Extensions.extensions:emit(Extensions.event_names.REMOVE, { list = list, item = item, idx = i })
+            break
+          end
+        end
+      end
+    end
 
     vim.keymap.set('n', '<leader>ha', function()
       harpoon:list():add()
     end, { desc = 'Add current buffer to Harpoon list' })
-    vim.keymap.set('n', '<leader>hd', function()
-      harpoon:list():remove()
-    end, { desc = 'Delete current buffer to Harpoon list' })
+    vim.keymap.set('n', '<leader>hd', remove, { desc = 'Delete current buffer to Harpoon list' })
 
     vim.keymap.set('n', '<leader>ho', function()
       harpoon.ui:toggle_quick_menu(harpoon:list())
