7d35b3b6f9c9e2a17e73f531f68b3ca47544e6b8
Isaiah Banks <isaiah.banks42@gmail.com>
2024-10-09 15:33:05 -0400

    disable accept suggestion for less smoothbrain

--- nvim/lua/custom/plugins/supermaven.lua
+++ nvim/lua/custom/plugins/supermaven.lua
@@ -3,18 +3,17 @@ return {
   config = function()
     require('supermaven-nvim').setup {
       keymaps = {
-        accept_suggestion = '<M-l>',
-        accept_word = '<M-k>',
+        accept_word = '<M-l>',
         clear_suggestion = '<M-j>',
       },
       ignore_filetypes = { cpp = true },
       color = {
-        suggestion_color = '#ffffff',
+        suggestion_color = vim.o.background == 'light' and '#000000' or '#ffffff',
         cterm = 244,
       },
-      log_level = 'info',                -- set to "off" to disable logging completely
+      log_level = 'off', -- set to "off" to disable logging completely
       disable_inline_completion = false, -- disables inline completion for use with cmp
-      disable_keymaps = false,           -- disables built in keymaps for more manual control
+      disable_keymaps = false, -- disables built in keymaps for more manual control
     }
   end,
 }
