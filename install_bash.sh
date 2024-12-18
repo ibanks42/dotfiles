1955d5c9ca29a98300cd8cf609e64bacc1dc1eb6
Isaiah Banks <isaiah.banks42@gmail.com>
2024-12-17 17:56:28 -0800

    it works now Confirmed with fresh install of linux mint on a VM. Will need to test ubuntu/fedora/the other distros but... it works for mint at least, so it should work for everything else.
    (it probably won't)

--- install_bash.sh
+++ install_bash.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -i
 
 # Variables
 REPO_URL="https://github.com/ibanks42/dotfiles.git"
@@ -10,18 +10,13 @@ NVIM_CACHE_PATH="$HOME/.cache/nvim"
 CWD=$(pwd)
 
 # Check for /etc/os-release
-if [ -f /etc/os-release ]; then
-  source /etc/os-release
-  DISTRO="$ID"
-else
-  # Fallback to lsb_release if /etc/os-release is not present
-  DISTRO=$(lsb_release -i | awk '{print $3}')
-fi
+source /etc/os-release
+DISTRO="$ID"
 
 update_packages() {
   case "$DISTRO" in
     "ubuntu" | "debian" | "linuxmint")
-      sudo apt update && sudo apt upgrade
+      sudo apt-get update && sudo apt-get upgrade
       ;;
     "fedora")
       sudo dnf update
@@ -35,47 +30,29 @@ update_packages() {
     "rhel" | "centos")
       sudo yum update
       ;;
-    *)
-      echo "-> Unsupported distribution for Git. Please install Git manually."
-      ;;
     esac
-
 }
 
 install_git() {
   if ! command -v git &>/dev/null; then
     echo "Installing Git..."
     case "$DISTRO" in
-    "ubuntu" | "debian" | "linuxmint")
-      sudo apt-get install -y git
-      ;;
-    "fedora")
-      sudo dnf install -y git
-      ;;
-    "arch")
-      sudo pacman -S git -y
-      ;;
-    "opensuse")
-      sudo zypper install -y git
-      ;;
-    "rhel" | "centos")
-      sudo yum install -y git
-      ;;
-    *)
-      echo "-> Unsupported distribution for Git. Please install Git manually."
-      ;;
+      "ubuntu" | "debian" | "linuxmint")
+        sudo apt-get install -y git
+        ;;
+      "fedora")
+        sudo dnf install -y git
+        ;;
+      "arch")
+        sudo pacman -S git -y
+        ;;
+      "opensuse")
+        sudo zypper install -y git
+        ;;
+      "rhel" | "centos")
+        sudo yum install -y git
+        ;;
     esac
-  fi
-
-  if ! command -v gh &>/dev/null; then
-    echo "-> Installing gh..."
-    GHVERSION=$(wget -q "https://api.github.com/repos/cli/cli/releases/latest" -O - | grep -Po '"tag_name": *"v\K[^"]*')
-    wget -qO gh.tar.gz "https://github.com/cli/cli/releases/download/v${GHVERSION}/gh_${GHVERSION}_linux_amd64.tar.gz"
-    tar xf gh.tar.gz
-    sudo install "gh_${GHVERSION}_linux_amd64/bin/gh" -D -t /usr/bin/
-    sudo cp -R "gh_${GHVERSION}_linux_amd64/share" /usr/local
-
-    gh auth login
   fi
 }
 
@@ -98,11 +75,26 @@ clone_repository() {
   echo "-> Repository cloned successfully."
 }
 
+install_gh() {
+  if ! command -v gh &>/dev/null; then
+    echo "-> Installing gh..."
+    GHVERSION=$(wget -q "https://api.github.com/repos/cli/cli/releases/latest" -O - | grep -Po '"tag_name": *"v\K[^"]*')
+    wget -qO gh.tar.gz "https://github.com/cli/cli/releases/download/v${GHVERSION}/gh_${GHVERSION}_linux_amd64.tar.gz"
+    tar xf gh.tar.gz
+    sudo install "gh_${GHVERSION}_linux_amd64/bin/gh" -D -t /usr/bin/
+    sudo cp -R "gh_${GHVERSION}_linux_amd64/share" /usr/local
+
+    rm -rf gh.tar.gz
+
+    gh auth login
+  fi
+}
+
 install_nvim() {
   if ! command -v nvim &>/dev/null; then
-    wget -q -O "$TEMP_PATH/nvim-linux64.tar.gz" "https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz"
     cd "$TEMP_PATH" || exit
-    tar -xf "$TEMP_PATH/nvim-linux64.tar.gz"
+    wget -q -O nvim-linux64.tar.gz "https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz"
+    tar -xf nvim-linux64.tar.gz
     sudo install nvim-linux64/bin/nvim /usr/local/bin/nvim
     sudo cp -R nvim-linux64/lib /usr/local/
     sudo cp -R nvim-linux64/share /usr/local
@@ -136,9 +128,9 @@ install_nvim() {
 install_zellij() {
   if ! command -v zellij &>/dev/null; then
     echo "-> Installing zellij..."
-    wget -q -O "$TEMP_PATH/zellij.tar.xz" "https://github.com/zellij-org/zellij/releases/latest/download/zellij-x86_64-unknown-linux-musl.tar.gz"
     cd "$TEMP_PATH" || exit
-    tar -xf "$TEMP_PATH/zellij.tar.xz"
+    wget -q -O "zellij.tar.xz" "https://github.com/zellij-org/zellij/releases/latest/download/zellij-x86_64-unknown-linux-musl.tar.gz"
+    tar -xf "zellij.tar.xz"
     sudo install zellij /usr/local/bin/zelli
     cd "$CWD" || exit
   fi
@@ -153,7 +145,7 @@ install_alacritty() {
     echo "-> Installing alacritty..."
 
     case "$DISTRO" in
-    "ubuntu" | "debian")
+    "ubuntu" | "debian" | "linuxmint")
       sudo apt-get install -y cmake g++ pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3 alacritty
       ;;
     "fedora")
@@ -164,9 +156,6 @@ install_alacritty() {
       ;;
     "opensuse")
       sudo zypper install -y cmake freetype-devel fontconfig-devel libxcb-devel libxkbcommon-devel alacritty
-      ;;
-    *)
-      echo "-> Unsupported distribution for alacritty. Please install alacritty manually."
       ;;
     esac
   fi
@@ -198,73 +187,91 @@ install_ideavim() {
 install_mise() {
   if ! command -v mise &>/dev/null; then
     # Install mise for managing multiple versions of languages. See https://mise.jdx.dev/
-    wget -O - https://mise.run | sh
-    tee -a "$HOME/.bashrc" <<<"eval \"$(~/.local/bin/mise activate bash)\""
-    ~/.local/bin/mise install --global node@latest
-    ~/.local/bin/mise use --global node@latest
+    wget -qO - https://mise.run | sh
+    tee -a "$HOME/.bashrc" <<<"eval \"\$(\$HOME/.local/bin/mise activate bash)\""
+    "$HOME/.local/bin/mise" use --global node@latest
   fi
 }
 
 setup_bash() {
   echo "-> Setting up bash and installing requirements..."
-  if ! command -v eva &>/dev/null; then
-    echo "-> Installing eva (ls alternative)..."
-    wget -c https://github.com/eza-community/eza/releases/latest/download/eza_x86_64-unknown-linux-gnu.tar.gz -O - | tar xz
+  if ! command -v eza &>/dev/null; then
+    echo "-> Installing eza (ls alternative)..."
+    cd "$TEMP_PATH" || exit
+    wget -qO eza.tar.gz "https://github.com/eza-community/eza/releases/latest/download/eza_x86_64-unknown-linux-gnu.tar.gz"
+    tar xf eza.tar.gz
     sudo install eza /usr/bin/eza
+    cd "$CWD" || exit
   fi
 
   if ! command -v fzf &>/dev/null; then
     echo "-> Installing fzf (telescope alternative)..."
-    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-    ~/.fzf/install
+    cd "$TEMP_PATH" || exit
+    FZFVERSION=$(wget -q "https://api.github.com/repos/junegunn/fzf/releases/latest" -O - | grep -Po '"tag_name": *"v\K[^"]*')
+    wget -qO fzf.tar.gz "https://github.com/junegunn/fzf/releases/download/v${FZFVERSION}/fzf-${FZFVERSION}-linux_amd64.tar.gz"
+    tar xf fzf.tar.gz fzf
+    sudo install fzf -D -t /usr/local/bin/
+    cd "$CWD" || exit
   fi
 
-  if ! command -v z &>/dev/null; then
+  if ! command -v zoxide &>/dev/null; then
     echo "-> Installing zoxide (cd alternative)..."
-    wget -qO- https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
-    tee -a ~/.bashrc <<<"eval \"(zoxide init bash)\""
+    cd "$TEMP_PATH" || exit
+    wget -qO- https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh >/dev/null 2>&1
+    tee -a "$HOME/.bashrc" <<<"export PATH=\"\$PATH:\$HOME/.local/bin\""
+    tee -a "$HOME/.bashrc" <<<"eval \"\$(zoxide init bash)\""
+    cd "$CWD" || exit
   fi
 
   if ! command -v fdfind &>/dev/null; then
     echo "-> Installing fd..."
     cd "$TEMP_PATH" || exit
-    FDVERSION=$(wget -q "https://api.github.com/repos/sharkdp/fd/releases/latest" -O - | jq -r '.tag_name' | sed 's/^v//')
+    FDVERSION=$(wget -q "https://api.github.com/repos/sharkdp/fd/releases/latest" -O - | grep -Po '"tag_name": *"v\K[^"]*')
     wget -qO fd.tar.gz "https://github.com/sharkdp/fd/releases/download/v${FDVERSION}/fd-v${FDVERSION}-x86_64-unknown-linux-musl.tar.gz"
     tar xf fd.tar.gz
     cp "fd-v${FDVERSION}-x86_64-unknown-linux-musl/fd" "fd-v${FDVERSION}-x86_64-unknown-linux-musl/fdfind"
     sudo install "fd-v${FDVERSION}-x86_64-unknown-linux-musl/fd" -D -t /usr/local/bin/
     sudo install "fd-v${FDVERSION}-x86_64-unknown-linux-musl/fdfind" -D -t /usr/local/bin/
+    cd "$CWD" || exit
   fi
 
   if ! command -v lazygit &>/dev/null; then
     echo "-> Installing lazygit..."
+    cd "$TEMP_PATH" || exit
     LAZYGIT_VERSION=$(wget -q "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" -O - | grep -Po '"tag_name": *"v\K[^"]*')
     wget -qO lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
     tar xf lazygit.tar.gz lazygit
     sudo install lazygit -D -t /usr/local/bin/
+    cd "$CWD" || exit
   fi
 
   echo "-> Setting aliases..."
-  tee -a "$HOME/.bashrc" <<<"alias ls='eza -lh --group-directories-first --icons'"
-  tee -a "$HOME/.bashrc" <<<"alias lsa='ls -a'"
-  tee -a "$HOME/.bashrc" <<<"alias la='ls -a'"
-  tee -a "$HOME/.bashrc" <<<"alias lt='eza --tree --level=2 --long --icons --git'"
-  tee -a "$HOME/.bashrc" <<<"alias lta='lt -a'"
-  tee -a "$HOME/.bashrc" --preview 'batcat --style=numbers --color=always {}'"" <<<"alias ff="fzf
-  tee -a "$HOME/.bashrc" <<<"alias fd='fdfind'"
-  tee -a "$HOME/.bashrc" <<<"alias cd='z'"
+  tee -a "$HOME/.bashrc" <<<"alias ls='eza -lh --group-directories-first --icons'" >/dev/null 2>&1
+  tee -a "$HOME/.bashrc" <<<"alias lsa='ls -a'" >/dev/null 2>&1
+  tee -a "$HOME/.bashrc" <<<"alias la='ls -a'" >/dev/null 2>&1
+  tee -a "$HOME/.bashrc" <<<"alias lt='eza --tree --level=2 --long --icons --git'" >/dev/null 2>&1
+  tee -a "$HOME/.bashrc" <<<"alias lta='lt -a'" >/dev/null 2>&1
+  tee -a "$HOME/.bashrc" <<<"alias ff=\"fzf --preview 'batcat --style=numbers --color=always {}'\"" >/dev/null 2>&1
+  tee -a "$HOME/.bashrc" <<<"alias fd='fdfind'" >/dev/null 2>&1
+  tee -a "$HOME/.bashrc" <<<"alias cd='z'" >/dev/null 2>&1
 
-  echo "-> Setting bash ps1..."
-  tee -a ~/.bashrc <<<"PS1='\[\e[0;35m\]\u@\h\[\e[0m\]:\[\e[0;32m\]\w\[\e[0m\]\\$ '"
+  echo "-> Setting bash ps1..." >/dev/null 2>&1
+  tee -a "$HOME/.bashrc" <<<"PS1='\[\e[0;35m\]\u@\h\[\e[0m\]:\[\e[0;32m\]\w\[\e[0m\]\\$ '" >/dev/null 2>&1
+
+  source "$HOME/.bashrc"
 }
 
 update_packages
 
 install_git
 
+install_gh
+
 clone_repository
 
 setup_bash
+
+install_mise
 
 install_nvim
 
@@ -278,6 +285,7 @@ install_ideavim
 
 echo "-> Cleaning up..."
 # Cleanup
-rm -rf "$TEMP_PATH"
+# rm -rf "$TEMP_PATH"
 
-echo "\nDone!"
+echo ""
+echo "Done!"
