c0935050dce09935070665378180f6fd183c280c
Isaiah Banks <isaiah.banks42@gmail.com>
2025-01-08 14:24:41 -0500

    update nvim

--- nvim/lua/plugins/config.lua
+++ nvim/lua/plugins/config.lua
@@ -50,7 +50,8 @@ return {
       quickfile = { enabled = true },
       statuscolumn = { enabled = true },
       words = { enabled = true },
-      scroll = { enabled = true },
+      scroll = { enabled = false },
+      inlay_hints = { enabled = false },
       dashboard = {
         formats = {
           key = function(item)
@@ -136,48 +137,6 @@ return {
           end,
           { section = "startup" },
         },
-      },
-    },
-  },
-
-  -- since `vim.tbl_deep_extend`, can only merge tables and not lists, the code above
-  -- would overwrite `ensure_installed` with the new value.
-  -- If you'd rather extend the default config, use the code below instead:
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      -- add tsx and treesitter
-      vim.list_extend(opts.ensure_installed, {
-        "bash",
-        "html",
-        "javascript",
-        "json",
-        "lua",
-        "markdown",
-        "markdown_inline",
-        "python",
-        "query",
-        "regex",
-        "tsx",
-        "typescript",
-        "vim",
-        "yaml",
-        "tsx",
-        "typescript",
-      })
-    end,
-  },
-
-  -- add any tools you want to have installed below
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "stylua",
-        "shellcheck",
-        "shfmt",
-        "flake8",
-        "biome",
       },
     },
   },
