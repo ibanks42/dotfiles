0c52e4650803070e327ff48bdc773a80b4116c0b
Isaiah Banks <isaiah.banks42@gmail.com>
2025-04-02 18:46:36 -0400

    add vscode theme

--- nvim/lua/custom/plugins/theme.lua
+++ nvim/lua/custom/plugins/theme.lua
@@ -28,53 +28,21 @@ local function write_theme_preference(theme)
   end
 end
 
--- Function to set cursor color based on theme
-local function set_cursor_color()
-  -- Reset guicursor to default first
-  -- vim.opt.guicursor = 'n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50'
-  -- vim.opt.guicursor:append 'a:blinkwait700-blinkoff400-blinkon250'
-  --
-  -- if vim.o.background == 'light' then
-  --   -- Semi-transparent dark cursor for light theme
-  --   -- Using a dark gray (#ababab) instead of pure black for less harshness
-  --
-  --   vim.cmd [[highlight Cursor guifg=black guibg=black]]
-  --   vim.cmd [[highlight iCursor guifg=black guibg=black]]
-  --   vim.cmd [[highlight vCursor guifg=black guibg=black]]
-  --   vim.cmd [[highlight lCursor guifg=black guibg=black]]
-  -- else
-  --   --
-  --   -- White cursor for dark theme
-  --   vim.cmd [[highlight Cursor guifg=black guibg=white]]
-  --   vim.cmd [[highlight iCursor guifg=black guibg=white]]
-  --   vim.cmd [[highlight vCursor guifg=black guibg=white]]
-  --   vim.cmd [[highlight lCursor guifg=black guibg=white]]
-  -- end
-
-  -- Append cursor styles using the defined highlight groups
-  -- vim.opt.guicursor:append 'n-v-c:block-Cursor/lCursor'
-  -- vim.opt.guicursor:append 'i-ci:ver25-iCursor/lCursor'
-  -- vim.opt.guicursor:append 'r-cr:hor20-Cursor/lCursor' -- Apply to replace mode too
-  -- vim.opt.guicursor:append 'o:hor50' -- Operator-pending mode
-end
-
 -- Function to apply theme
 local function apply_theme(theme)
-  vim.notify(theme)
   if theme == 'light' then
     vim.o.background = 'light'
     -- require('carbide').apply 'light'
     vim.cmd 'colorscheme daylight'
-    set_cursor_color()
   elseif theme == 'catppuccin' then
     vim.o.background = 'dark'
     vim.cmd 'colorscheme catppuccin-macchiato'
-    set_cursor_color()
-    -- hi('@tag.attribute', { fg = c.Grey3.fg, bold = true })
+  elseif theme == 'vscode' then
+    vim.o.background = 'dark'
+    require('vscode').load 'dark'
   elseif theme == 'rose-pine' then
     vim.o.background = 'dark'
     vim.cmd 'colorscheme rose-pine'
-    set_cursor_color()
   end
 
   -- Force redraw to apply cursor changes if needed
@@ -93,6 +61,9 @@ vim.keymap.set('n', '<leader>ut', function()
   elseif current_theme == 'catppuccin' then
     apply_theme 'rose-pine'
     write_theme_preference 'rose-pine'
+  elseif current_theme == 'rose-pine' then
+    apply_theme 'vscode'
+    write_theme_preference 'vscode'
   else
     apply_theme 'light'
     write_theme_preference 'light'
@@ -105,18 +76,6 @@ vim.api.nvim_create_autocmd('User', {
   callback = function()
     local saved_theme = read_theme_preference()
     apply_theme(saved_theme)
-    -- Create an autocmd to ensure cursor color persists after colorscheme changes
-    vim.api.nvim_create_autocmd('ColorScheme', {
-      pattern = '*',
-      callback = function()
-        -- Give Neovim a moment to fully apply the colorscheme before setting cursor
-        vim.defer_fn(function()
-          local is_dark = (vim.o.background == 'dark')
-          set_cursor_color(is_dark)
-        end, 50) -- 50ms delay, adjust if needed
-      end,
-      desc = 'Reapply custom cursor color on colorscheme change',
-    })
   end,
 })
 
@@ -128,6 +87,13 @@ return {
     priority = 1000,
     config = function()
       require('catppuccin').setup()
+    end,
+  },
+  {
+    'Mofiqul/vscode.nvim',
+    priority = 1000,
+    config = function()
+      require('vscode').setup {}
     end,
   },
   {
