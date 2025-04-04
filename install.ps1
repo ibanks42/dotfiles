14314cf872fc77a624bbda7409d20690dcadbecb
Isaiah Banks <isaiah.banks42@gmail.com>
2025-04-04 19:34:55 -0400

    remove wezterm

--- install.ps1
+++ install.ps1
@@ -2,17 +2,6 @@
 $repoUrl = "https://github.com/ibanks42/dotfiles.git"
 $tempPath = "$env:TEMP\nvim-install"
 $nvimConfigPath = "$env:LOCALAPPDATA\nvim"
-$weztermConfigPath = "$env:USERPROFILE"
-
-function Install-Wezterm {
-    $weztermInstalled = winget list --name wezterm
-    if ($weztermInstalled -like "*WezTerm*") {
-	Write-Host "-> Wezterm is installed."
-    } else {
-	Write-Host "-> Wezterm is not installed. Installing Wezterm..."
-	winget install wez.wezterm
-    }
-}
 
 function Install-Neovim {
     $nvimInstalled = winget list --name neovim
@@ -52,16 +41,6 @@ function Copy-NvimConfig {
     Write-Host "-> Neovim configuration installed successfully."
 }
 
-function Copy-WeztermConfig {
-    if (Test-Path -Path "$env:HOME\.wezterm.lua") {
-        Remove-Item -Recurse -Force -Path "$env:HOME\.wezterm.lua"
-    }
-
-    Copy-Item -Recurse -Force -Path "$tempPath\wezterm\*" -Destination $weztermConfigPath
-
-    Write-Host "-> Wezterm configuration installed successfully."
-}
-
 function Install-Fonts {
     Write-Host "-> Installing fonts..."
 
@@ -89,27 +68,16 @@ try {
         exit 1
     }
 
-    # Check if Wezterm is installed
-    Install-Wezterm
-
-    # Check if Neovim is installed
     Install-Neovim
 
-    # Clone the repository
     Get-Repository
 
-    # Copy nvim configuration
     Copy-NvimConfig
-
-    # Copy Wezterm configuration
-    Copy-WeztermConfig
 
     Copy-Ideavim
 
-    # Install fonts
     Install-Fonts
 
-    # Copilot, put a checkmark at the beginning of the string
     Write-Host "✅ Installation complete!"
 
 } catch {
