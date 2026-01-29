36f0c89f9d7acf260819b7d5fac0bab8fc1dd898
Isaiah Banks <isaiah.banks42@gmail.com>
2026-01-29 09:10:24 -0500

    update nvim to use lazyvim

--- nvim/lua/custom/plugins/buvvers.lua
+++
@@ -1,7 +0,0 @@
-return {
-  'aidancz/buvvers.nvim',
-  dependencies = { { 'echasnovski/mini.bufremove', version = false, opts = {} } },
-  config = function()
-    require('buvvers').setup {}
-  end,
-}
--- nvim/lua/custom/plugins/undotree.lua
+++
@@ -1,7 +0,0 @@
-if vim.g.vscode then
-  return {}
-end
-
-vim.keymap.set('n', '<leader>cu', vim.cmd.UndotreeToggle, { desc = '[U]ndo Tree' })
-
-return { 'mbbill/undotree' }
