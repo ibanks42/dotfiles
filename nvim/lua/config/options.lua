e770ce424fd625510136c02dd0962dfd3e10f979
Isaiah Banks <isaiah.banks42@gmail.com>
2026-03-01 19:34:55 -0500

    enable OCS52 copy/paste for SSH

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,34 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+-- Explicitly configure the clipboard provider to use OSC 52
+
+vim.opt.clipboard = nil
+
+function unnamed_paste(reg)
+  return function(lines)
+    local content = vim.fn.getreg('"')
+    return vim.split(content, "\n")
+  end
+end
+
+vim.g.clipboard = {
+  name = "dummy clipboard",
+  copy = {
+    ["+"] = function(lines) end,
+    ["*"] = function(lines) end,
+  },
+  paste = {
+    ["+"] = unnamed_paste("+"),
+    ["*"] = unnamed_paste("*"),
+  },
+}
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+  callback = function()
+    local copy_to_unnamedplus = require("vim.ui.clipboard.osc52").copy("+")
+    copy_to_unnamedplus(vim.v.event.regcontents)
+    local copy_to_unnamed = require("vim.ui.clipboard.osc52").copy("*")
+    copy_to_unnamed(vim.v.event.regcontents)
+  end,
+})
