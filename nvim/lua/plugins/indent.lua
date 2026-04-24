34a592d35de06b598a9263388d5f60941872d18d
Isaiah Banks <isaiah.banks42@gmail.com>
2026-04-24 12:46:36 -0400

    nvim stuff

--- nvim/lua/plugins/indent.lua
+++ nvim/lua/plugins/indent.lua
@@ -2,9 +2,6 @@ return {
   {
     "lukas-reineke/indent-blankline.nvim",
     main = "ibl",
-    ---@module "ibl"
-    ---@type ibl.config
-    opts = {},
     config = function()
       local highlight = {
         "RainbowRed",
@@ -17,8 +14,7 @@ return {
       }
 
       local hooks = require("ibl.hooks")
-      -- create the highlight groups in the highlight setup hook, so they are reset
-      -- every time the colorscheme changes
+
       hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
         vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
         vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
@@ -28,13 +24,18 @@ return {
         vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
         vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
       end)
+
       require("ibl").setup({
-        indent = { highlight = highlight, char = "¦" },
-        whitespace = {
+        indent = {
           highlight = highlight,
-          remove_blankline_trail = false,
+          -- or "│", "¦", "╎"
+          char = "┊", -- spaces
+          tab_char = "┊", -- tabs
         },
-        scope = { enabled = true },
+        whitespace = {
+          remove_blankline_trail = true,
+        },
+        scope = { enabled = false },
       })
     end,
   },
